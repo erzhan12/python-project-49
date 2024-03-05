306aac673e77350b3535a3aec08b51117012ea89
erzhan <erzhan.sarsenov@gmail.com>
2024-03-05 23:01:11 +0500

    step 4

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-import prompt
 import brain_games.cli
+
+
 def main():
-  brain_games.cli.welcome_user()
+    brain_games.cli.welcome_user()
 
 
 if __name__ == "__main__":
-  main()
+    main()

48480597d54127391ff52e223a28e88b7ee00245
erzhan <erzhan.sarsenov@gmail.com>
2024-03-05 23:35:20 +0500

    step 5.1

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -22,7 +22,8 @@ def game(name):
         print('Correct!')
         return False
     else:
-        print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+        print(f"'{user_answer}' is wrong answer ;(. "
+              f"Correct answer was '{correct_answer}'.")
         print(f"Let's try again, {name}")
         return True
 

fede5aec178d9378411706b9eb7a78d11c3e0c70
erzhan <erzhan.sarsenov@gmail.com>
2024-09-12 01:18:28 +0500

    step 9.4

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -17,7 +17,7 @@ def start_game(generate_round_data, question_text, number_of_rounds=3):
         if user_answer != correct_answer:
             print(f"'{user_answer}' is wrong answer ;(."
                   f" Correct answer was '{correct_answer}'")
-            print(f"Let's try again, {name}")
+            print(f"Let's try again, {name}!")
             return
         print("Correct")
     print(f"Congratulations, {name}!")
